b1f9fa24aa5765cf1d2bc043c5ece275f9bcae6f
Lotar <amaral.lucas.lotar#gmail.com>
2025-11-20 10:53:56 -0300

    criado uri2609 SQL e JPQL

--- uri2609/src/main/java/com/devsuperior/uri2609/Uri2609Application.java
+++ uri2609/src/main/java/com/devsuperior/uri2609/Uri2609Application.java
@@ -1,11 +1,15 @@
 package com.devsuperior.uri2609;
 
+import com.devsuperior.uri2609.dto.CategorySumDTO;
+import com.devsuperior.uri2609.projections.CategorySumProjection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.devsuperior.uri2609.repositories.CategoryRepository;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Uri2609Application implements CommandLineRunner {
@@ -19,6 +23,22 @@ public class Uri2609Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+
+    List<CategorySumProjection> list = repository.search1();
+    List<CategorySumDTO> listDTO = list.stream().map(CategorySumDTO::new).toList();
+
+    System.out.println("RESULTADO SQL");
+    for (CategorySumDTO dto : listDTO) {
+      System.out.println(dto);
+    }
+
+    System.out.println("\n\n");
+
+    List<CategorySumDTO> list2 = repository.search2();
+    System.out.println("RESULTADO JPQL");
+    for (CategorySumDTO dto : list2) {
+      System.out.println(dto);
+    }
 
 	}
 }
